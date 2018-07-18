5da112c2540de9deab15ec11fe2a2c6d8070ee0c
ashwini <ashwini.mandal@cuelogic.com>
2018-07-18 17:23:44 +0530

    ingredients added with state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,23 @@ import classes from './Burger.css'
 import Burger_Ingredient from './Burger_ingredients/Burger_ingredients'
 
 const burger = (props) => {
+        const new_ingredients = Object.keys(props.ingredients)
+        const transformed_ingredients = new_ingredients.map(keys=>{
+            return [...Array(props.ingredients[keys])].map((_,i) =>{
+                return <Burger_Ingredient type={keys} key={keys+i}/>
+            })
 
+        })
+
+            
+
+        console.log("------------new Ingredients------------",{new_ingredients})
+        console.log("------------Ingredients------------",{transformed_ingredients})
+        
         return(
             <div className={classes.Burger}>
             <Burger_Ingredient type="bread-top"/>
-            <Burger_Ingredient type="cheese"/>
-            <Burger_Ingredient type="meat"/>
+            {transformed_ingredients}
             <Burger_Ingredient type="bread-bottom"/>
             </div>
         )
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,18 @@ import Aux from '../../hoc/Aux'
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component{
+    state = {
+        ingredients :{
+            cheese : 1,
+            salad :2,
+            meat : 2,
+            becon : 2
+        }
+    }
     render(){
         return(
             <Aux>
-            <Burger/>
+            <Burger ingredients={this.state.ingredients}/>
             <div>Burger Control</div>
             </Aux>
         )
